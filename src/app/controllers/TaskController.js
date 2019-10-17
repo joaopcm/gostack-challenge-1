c21e3dfeec0ad066178ef6466902bab5627bf218
João Pedro da Cruz Melo <jopcmelo@gmail.com>
2019-10-17 15:04:34 -0300

    chore: Add task route

--- src/app/controllers/TaskController.js
+++ src/app/controllers/TaskController.js
@@ -4,15 +4,30 @@ import Project from '../schemas/Project';
 
 class TaskController {
   async store(req, res) {
-    // const schema = Yup.object().shape({
-    //   title: Yup.string().required(),
-    // });
+    const schema = Yup.object().shape({
+      id: Yup.string()
+        .required()
+        .length(24),
+      tasks: Yup.string().required(),
+    });
 
-    // if (!(await schema.isValid(req.body))) {
-    //   return res.status(400).json({ error: 'Validation failed' });
-    // }
+    if (!(await schema.isValid({ ...req.body, ...req.params }))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
 
-    return res.json({ ok: true });
+    const { id } = req.params;
+    const { tasks } = req.body;
+
+    const project = await Project.findById(id);
+
+    project.tasks = [
+      ...project.tasks,
+      ...tasks.split(',').map(task => task.trim()),
+    ];
+
+    project.save();
+
+    return res.json(project);
   }
 }
 
